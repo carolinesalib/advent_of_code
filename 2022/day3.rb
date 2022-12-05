e8cdf2eeee7900792e3adc743d14b8db69c22ab3
Caroline Salib <carolinesalibc@gmail.com>
2022-12-05 08:37:18 -0700

    Day 3 part 2

--- 2022/day3.rb
+++ 2022/day3.rb
@@ -11,23 +11,31 @@ file = File.open("day3_input.txt")
 file_data = file.read
 input = file_data.split("\n")
 
-result = 0
-
-input.each do |rucksack|
-  compartment_1 = rucksack.slice(0, rucksack.size / 2)
-  compartment_2 = rucksack.slice(rucksack.size / 2, rucksack.size)
-
-  # p "#{compartment_1} - #{compartment_2}"
-
-  shared_item = (compartment_1.chars & compartment_2.chars).first
-
-  # p shared_item
-
-  item_alphabetic_index = (("a".."z").to_a + ("A".."Z").to_a).find_index(shared_item) + 1
-
-  # p item_alphabetic_index
-
-  result += item_alphabetic_index
+def item_alphabetic_index(item)
+  (("a".."z").to_a + ("A".."Z").to_a).find_index(item) + 1
 end
 
-p "Result: #{result}"
+result_part1 = 0
+result_part2 = 0
+
+input.each do |rucksack|
+  compartment1 = rucksack.slice(0, rucksack.size / 2)
+  compartment2 = rucksack.slice(rucksack.size / 2, rucksack.size)
+  # p "#{compartment1} - #{compartment2}"
+  shared_item = (compartment1.chars & compartment2.chars).first
+  # p shared_item
+  item_alphabetic_index = (("a".."z").to_a + ("A".."Z").to_a).find_index(shared_item) + 1
+  # p item_alphabetic_index
+  result_part1 += item_alphabetic_index
+end
+
+p "Result part 1: #{result_part1}"
+
+input.each_slice(3) do |rucksack1, rucksack2, rucksack3|
+  # p rucksack1, rucksack2, rucksack3
+  shared_item = (rucksack1.chars & rucksack2.chars & rucksack3.chars).first
+  # p "#{shared_item}"
+  result_part2 += item_alphabetic_index(shared_item)
+end
+
+p "Result part 2: #{result_part2}"
