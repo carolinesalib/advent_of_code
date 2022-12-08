884857bd6f68b9d4ef76f1095d9f59f295addc2e
Caroline Salib <carolinesalibc@gmail.com>
2022-12-08 08:33:28 -0700

    day 6 part 2

--- 2022/day6.rb
+++ 2022/day6.rb
@@ -1,13 +1,14 @@
 file = File.open("day6_input.txt")
 input = file.read
+MARKER = 14
 
 four_different_chars = []
 
 input.chars.each_with_index do |char, index|
   four_different_chars << char
 
-  if four_different_chars.size == 4
-    if four_different_chars.uniq.size == 4
+  if four_different_chars.size == MARKER
+    if four_different_chars.uniq.size == MARKER
       p "Result: #{index + 1}"
       break
     else
