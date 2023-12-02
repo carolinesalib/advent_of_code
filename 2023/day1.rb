d4d22c929f036a6c982f8de4570a6f82b7feedaf
Caroline <carolinesalibc@gmail.com>
2023-12-01 22:42:16 -0700

    Day 1 part 2

--- 2023/day1.rb
+++ 2023/day1.rb
@@ -4,9 +4,41 @@ file = File.open("day1_input.txt")
 file_data = file.read
 input_lines = file_data.split("\n")
 
+def convert_text_to_numbers(text)
+  text_numbers = {
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9,
+  }
+
+  numbers = []
+
+  (0...text.split("").size).each do |index|
+    if text[index].gsub(/[0-9]/, "").empty?
+      numbers << text[index].to_i
+      next
+    end
+
+    text_numbers.each do |text_number, number|
+      if text[index, text_number.size] == text_number
+        numbers << number
+        break
+      end
+    end
+  end
+
+  numbers
+end
+
 result = input_lines.sum do |input|
-  numbers = input.gsub(/[^0-9]/, "").split("")
-  (numbers.first + numbers.last).to_i
+  numbers = convert_text_to_numbers(input)
+  "#{numbers.first}#{numbers.last}".to_i
 end
 
 puts result
