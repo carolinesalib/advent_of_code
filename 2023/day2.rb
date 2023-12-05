25ae52609593b2994c554c8322a1f0dea1ac15f7
Caroline <carolinesalibc@gmail.com>
2023-12-04 18:33:45 -0700

    Day 2 part 2

--- 2023/day2.rb
+++ 2023/day2.rb
@@ -4,41 +4,30 @@ file = File.open("day2_input.txt")
 file_data = file.read
 input_lines = file_data.split("\n")
 
-AVAILABLE_RED_CUBES = 12
-AVAILABLE_GREEN_CUBES = 13
-AVAILABLE_BLUE_CUBES = 14
-
-def enough_cubes?(game_cubes)
-  game_cubes.split(", ").each do |cubes|
-    number_of_cubes = cubes.strip.split(" ").first.to_i
-    cube_colour = cubes.strip.split(" ").last
-
-    if cube_colour == "red" && number_of_cubes > AVAILABLE_RED_CUBES ||
-        cube_colour == "green" && number_of_cubes > AVAILABLE_GREEN_CUBES ||
-        cube_colour == "blue" && number_of_cubes > AVAILABLE_BLUE_CUBES
-      return false
-    end
-  end
-
-  true
-end
-
 result = 0
 
 input_lines.each do |game|
-  game_number = game.split(": ").first.gsub(/\D/, "").to_i
   game_line = game.split(": ").last
 
-  enough_cubes_game = true
+  max_number_of_cubes_from_each_colour = {
+    "red" => 0,
+    "green" => 0,
+    "blue" => 0,
+  }
 
   game_line.split(";").each do |game_cubes|
-    unless enough_cubes?(game_cubes)
-      enough_cubes_game = false
-      break
+    hash_games = game_cubes.split(",").map(&:strip).map do |game_cube_line|
+      [game_cube_line.split(" ").last, game_cube_line.split(" ").first.to_i]
+    end
+
+    hash_games.map do |cube_type, number_cubes|
+      if number_cubes > max_number_of_cubes_from_each_colour[cube_type]
+        max_number_of_cubes_from_each_colour[cube_type] = number_cubes
+      end
     end
   end
 
-  enough_cubes_game && result += game_number
+  result += max_number_of_cubes_from_each_colour.values.inject(:*)
 end
 
 puts "Result: #{result}"
